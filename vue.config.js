70ff3c0ee29804de4407f8054eefd91c4331b431
datou <1392767180@qq.com>
2020-03-03 14:03:08 +0800

    chore: tree-shaking && import vue from cdn

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
 module.exports = {
-  publicPath: './'
+  publicPath: './',
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new TerserWebpackPlugin({
+          parallel: true  // 开启多进程打包 js
+        })
+      ]
+    },
+    externals: {
+      'vue': 'Vue',
+    }
+  }
 }
